50f3bdbd9b1da13575f5f5afe8b88f7f7043ca48
Harsh Anand <harsh250600@gmail.com>
2024-07-01 15:38:03 +0530

    Browser router component added

--- employee-management/src/App.jsx
+++ employee-management/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from './components/dashboard';
 
 function App() {
 
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/adminlogin' element={<Login />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
