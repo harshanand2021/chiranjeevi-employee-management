3095829da2a97a6b4f409b65eb52f2a29b4993c7
Harsh Anand <harsh250600@gmail.com>
2024-07-03 12:00:00 +0530

    Routes added

--- employee-management/src/App.jsx
+++ employee-management/src/App.jsx
@@ -1,19 +1,31 @@
-import './App.css'
-import Login from './components/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Dashboard from './components/dashboard';
+import "./App.css";
+import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Employee from "./components/Employee";
+import Category from "./components/Category";
+import Profile from "./components/Profile";
+import AddCategory from "./components/AddCategory";
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/adminlogin' element={<Login />}></Route>
-        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path="/adminlogin" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/dashboard/employee" element={<Employee />}></Route>
+          <Route path="/dashboard/category" element={<Category />}></Route>
+          <Route path="/dashboard/profile" element={<Profile />}></Route>
+          <Route path="/dashboard/add_category" element={<AddCategory />}></Route>
+          <Route path="/dashboard/add_employee" element={<AddEmployee />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
