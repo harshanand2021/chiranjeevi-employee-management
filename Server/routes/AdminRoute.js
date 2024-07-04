0494f94c1a29c5da556a380b7acb3743840756d0
Harsh Anand <harsh250600@gmail.com>
2024-07-04 14:40:32 +0530

    new get and post methods added

--- Server/routes/AdminRoute.js
+++ Server/routes/AdminRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import con from "../utils/db.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const router = express.Router()
 
@@ -20,7 +21,45 @@ router.post('/admin', (req,res) => {
             return res.json({loginStatus: false, Error: "Invalid email or password"});
         }
     })
+})
 
+router.post('/add_category',(req,res) => {
+  const sql = 'INSERT INTO category(`name`) VALUES(?)';
+  con.query(sql, [req.body.category], (err,result) => {
+    if(err)
+      return res.json({Status: false, Error: "Query Error"})
+    return res.json({Status: true})
+  })
+});
+
+router.get('/category',(req,res) => {
+  const sql = 'SELECT * FROM category';
+  con.query(sql, (err,result) => {
+    if(res)
+      return res.json({Status: false, Error: "Query Error"})
+    return res.json({Status: true, Result: result})
+  })
+})
+
+router.post('/add_employee',(req,res) => {
+  const sql = 'INSERT INTO employee(`name`,`email`,`password`,`salary`,`address`,`category_id`) VALUES(?)';
+  bcrypt.hash(req.body.password, 10, (err,hash) => {
+    if(err)
+      return res.json({Ststus: false, Error: "Query Error"})
+    const values = [
+      req.body.name,
+      req.body.email,
+      hash,
+      req.body.salary,
+      req.body.image,
+      req.body.category_id
+    ]
+    con.query(sql, values, (err,result) => {
+      if(err)
+        return res.json({Status: false, Error: "Query Error"})
+      return res.json({Status:true})
+    })
+  })
 })
 
 export {router as adminRouter}
